a5134403c33281a0d4d23076f2984d707a0fcb94
Nando Basilio <fernando_basilio@econeteditora.com.br>
2021-10-06 17:26:01 -0300

    show letras

--- digital/mostrador.php
+++ digital/mostrador.php
@@ -20,49 +20,34 @@ class Mostrador
 
     private function escrevePalavra(){
 
-        $linha_led_a[0]=' __';
-        $linha_led_a[1]='|__|';
-        $linha_led_a[2]='|  |';
-
-        $linha_led_b[0]=' __';
-        $linha_led_b[1]='|__|';
-        $linha_led_b[2]='|__/';
+        $linha_led_a = $this->getLetra('a');
+        $linha_led_b = $this->getLetra('b');
+        $linha_led_c = $this->getLetra('c');
 
         $lista = [0=>[], 1=>[], 2=>[]];
-        $lista[0][]=$linha_led_a[0];        
-        $lista[0][]=$linha_led_b[0];
 
-        /*$lista[2][]=$linha_led_a[1];
+        $lista[0][]=$linha_led_a[0]; 
+        $lista[0][]=$linha_led_b[0]; 
+        $lista[0][]=$linha_led_c[0]; 
+        $lista[1][]=$linha_led_a[1]; 
         $lista[1][]=$linha_led_b[1];
-
-        $lista[1][]=$linha_led_a[2];
-        $lista[2][]=$linha_led_b[2];*/
+        $lista[1][]=$linha_led_c[1];  
+        $lista[2][]=$linha_led_a[2]; 
+        $lista[2][]=$linha_led_b[2];
+        $lista[2][]=$linha_led_c[2];
 
         foreach($lista as $key=>$digitos){
             
             foreach($digitos as $key_num_digito=>$digito){
-                echo $digito."   |   ";
-                echo $key_num_digito;
+                echo $digito;
             }
             echo "\n";
-            //echo "\n";//quebra proxima linha
         }
-
-    }
-
-    private function getArrayLeds(){
-
-
-        $linha_led['1']=' __';
-        $linha_led['2']='|__|';
-        $linha_led['3']='|  |';
 
     }
 
     private function getLetra($p_letra){
 
-
-        $linhas['a']=[];
         $linhas['a'][]=' __';
         $linhas['a'][]='|__|';
         $linhas['a'][]='|  |';
